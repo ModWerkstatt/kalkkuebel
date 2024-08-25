23f4bf7cddd4b035a19e323c22f04c57a6b286f1
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-08-25 23:38:23 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -2,13 +2,16 @@ function data()
 return {
 	en = {
         ["mod_name"] = "DRG/DB Chalk Bucket Car",
-		["mod_desc"] = "Chalk bucket cars have been required for chemic industry for a long time. Nearly every big company had their own waggons. Lot of them have been in use until the 1980's.",
-		["kalkmB_name"] = "Chalk Bucket Car DRG w brakeman's cab",
-		["kalkmB_desc"] = "A typically chalk bucket cart with open platform. Waggon with brakeman's cab.",
-        ["kalk_name"] = "Chalk Bucket Car DB",
-		["kalk_desc"] = "A typically chalk bucket cart with open platform.",
-		["kalk_db_name"] = "Chalk Bucket Car DRG",
-		["kalk_db_desc"] = "A chalk bucket car of DB originally built by DRG with open platform and hand brake.",
+		["mod_desc"] = 	"Chalk bucket cars have been required for chemic industry for a long time. Nearly every big company had their own waggons. Lot of them have been in use until the 1980's.",
+		
+		["kalkkuebel_menu"] = "DRG/DB Chalk Bucket Car",
+		
+		["kalkmB_name"] = 	"Chalk Bucket Car DRG w brakeman's cab",
+		["kalkmB_desc"] = 	"A typically chalk bucket cart with open platform. Waggon with brakeman's cab.",
+        ["kalk_name"] = 	"Chalk Bucket Car DB",
+		["kalk_desc"] = 	"A typically chalk bucket cart with open platform.",
+		["kalk_db_name"] = 	"Chalk Bucket Car DRG",
+		["kalk_db_desc"] = 	"A chalk bucket car of DB originally built by DRG with open platform and hand brake.",
 	},
 	de = {
         ["mod_name"] = "DRG/DB Kalkkübelwagen",
@@ -16,12 +19,12 @@ return {
 		
 		["kalkkuebel_menu"] = "DRG/DB Kalkkübelwagen",
 		
-		["kalkmB_name"] = "Kalkkübelwagen DRG mit Bremserhaus",
-		["kalkmB_desc"] = "Ein Kalkkübelwagen, wie er typisch war für die Zeit. Wagen mit geschlossenem Bremserhaus.",
-        ["kalk_name"] = "Kalkkübelwagen DB",
-		["kalk_desc"] = "Ein Kalkkübelwagen, wie er typisch war für die Zeit.",
-        ["kalk_db_name"] = "Kalkkübelwagen DRG",
-		["kalk_db_desc"] = "Ein Kalkkübelwagen der DB, der aus Beständen der DRG übernommen wurde mit offener Bühne und Handbremse.",
+		["kalkmB_name"] = 	"Kalkkübelwagen DRG mit Bremserhaus",
+		["kalkmB_desc"] = 	"Ein Kalkkübelwagen, wie er typisch war für die Zeit. Wagen mit geschlossenem Bremserhaus.",
+        ["kalk_name"] = 	"Kalkkübelwagen DB",
+		["kalk_desc"] = 	"Ein Kalkkübelwagen, wie er typisch war für die Zeit.",
+        ["kalk_db_name"] = 	"Kalkkübelwagen DRG",
+		["kalk_db_desc"] = 	"Ein Kalkkübelwagen der DB, der aus Beständen der DRG übernommen wurde mit offener Bühne und Handbremse.",
 	}
 }
 end
