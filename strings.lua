eda71a15ede934b064183435f7f68553dbcb54b8
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-08-25 23:31:35 +0200

    Update 1.1
    Umbau auf Basis Mod
    - Sound
    - Zg2 Tafeln
    - Kaufmenü hinzugefügt(UI Bild fehlt noch)

--- strings.lua
+++ strings.lua
@@ -1,24 +1,27 @@
 function data()
 return {
 	en = {
-        mod_name = "DRG/DB Chalk Bucket Car",
-		mod_desc = "Chalk bucket cars have been required for chemic industry for a long time. Nearly every big company had their own waggons. Lot of them have been in use until the 1980's.",
-		kalkmB_name = "Chalk Bucket Car w brakeman's cab",
-		kalkmB_desc = "A typically chalk bucket cart with open platform. Waggon with brakeman's cab.",
-        kalk_name = "Chalk Bucket Car",
-		kalk_desc = "A typically chalk bucket cart with open platform.",
-        kalk_db_name = "Chalk Bucket Car",
-		kalk_db_desc = "A chalk bucket car of DB originally built by DRG with open platform and hand brake.",
+        ["mod_name"] = "DRG/DB Chalk Bucket Car",
+		["mod_desc"] = "Chalk bucket cars have been required for chemic industry for a long time. Nearly every big company had their own waggons. Lot of them have been in use until the 1980's.",
+		["kalkmB_name"] = "Chalk Bucket Car DRG w brakeman's cab",
+		["kalkmB_desc"] = "A typically chalk bucket cart with open platform. Waggon with brakeman's cab.",
+        ["kalk_name"] = "Chalk Bucket Car DB",
+		["kalk_desc"] = "A typically chalk bucket cart with open platform.",
+		["kalk_db_name"] = "Chalk Bucket Car DRG",
+		["kalk_db_desc"] = "A chalk bucket car of DB originally built by DRG with open platform and hand brake.",
 	},
 	de = {
-        mod_name = "DRG/DB Kalkkübelwagen",
-		mod_desc = "Kalkkübelwagen waren zum Transport von Kalk lange Zeit unabdingbar in der chemischen Industrie. Nahezu jede große Industrie hatte ihren eigenen Fuhrpark. Die Wagen waren bis in die 80er Jahre im Einsatz.",
-		kalkmB_name = "Kalkkübelwagen mit Bremserhaus",
-		kalkmB_desc = "Ein Kalkkübelwagen, wie er typisch war für die Zeit. Wagen mit geschlossenem Bremserhaus.",
-        kalk_name = "Kalkkübelwagen",
-		kalk_desc = "Ein Kalkkübelwagen, wie er typisch war für die Zeit.",
-        kalk_db_name = "Kalkkübelwagen",
-		kalk_db_desc = "Ein Kalkkübelwagen der DB, der aus Beständen der DRG übernommen wurde mit offener Bühne und Handbremse.",
+        ["mod_name"] = "DRG/DB Kalkkübelwagen",
+		["mod_desc"] = "Kalkkübelwagen waren zum Transport von Kalk lange Zeit unabdingbar in der chemischen Industrie. Nahezu jede große Industrie hatte ihren eigenen Fuhrpark. Die Wagen waren bis in die 80er Jahre im Einsatz.",
+		
+		["kalkkuebel_menu"] = "DRG/DB Kalkkübelwagen",
+		
+		["kalkmB_name"] = "Kalkkübelwagen DRG mit Bremserhaus",
+		["kalkmB_desc"] = "Ein Kalkkübelwagen, wie er typisch war für die Zeit. Wagen mit geschlossenem Bremserhaus.",
+        ["kalk_name"] = "Kalkkübelwagen DB",
+		["kalk_desc"] = "Ein Kalkkübelwagen, wie er typisch war für die Zeit.",
+        ["kalk_db_name"] = "Kalkkübelwagen DRG",
+		["kalk_db_desc"] = "Ein Kalkkübelwagen der DB, der aus Beständen der DRG übernommen wurde mit offener Bühne und Handbremse.",
 	}
 }
 end
