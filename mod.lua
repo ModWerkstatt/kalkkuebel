38b0949f4768b3e370cbbbb5a5b344c1692a5844
Florian Thiel <flth@flth.de>
2024-08-25 22:28:48 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,24 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "europe", "germany", "waggon", "construction materials", "stone", "Bundesbahn" },
+	},
+	options = {
+		},
+
+		runFn = function (settings)
+
+		end
+	}
+end
